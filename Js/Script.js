f4a707ecdcfbe0b08053148e854b6b4699d8cf42
SlayerrR18 <yoseph.sakera@binus.ac.id>
2023-12-15 22:28:35 +0700

    membuat website kost lolia

---
+++ Js/Script.js
@@ -0,0 +1,15 @@
+// class active
+
+const isinavbar = document.querySelector(".isi-navbar");
+document.querySelector("#hamburger-menu").onclick = () => {
+  isinavbar.classList.toggle("active");
+};
+
+//klik diluar navbar untuk menutup isi navbar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !isinavbar.contains(e.target)) {
+    isinavbar.classList.remove("active");
+  }
+});
