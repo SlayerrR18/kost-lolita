a1883ccec85ab7db880e067e21be90ae859e9d49
SlayerrR18 <122510689+SlayerrR18@users.noreply.github.com>
2025-05-25 11:13:01 +0700

    push tiga

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
-
-
-
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('/admin/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware('auth', 'is_admin');
-Route::get('/user/dashboard', function () {
-    return view('user.dashboard');
-
-
-})->middleware('auth');
 
 Route::get('/', function () {
     return view('dashboard');
